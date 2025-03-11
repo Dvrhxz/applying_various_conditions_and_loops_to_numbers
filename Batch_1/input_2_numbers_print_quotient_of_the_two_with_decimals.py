c131741b5590838d290002a24ba1a4e4a6a4ef7c
my name <you@example.com>
2025-03-12 00:29:34 +0800

    2 number inputs

--- Batch_1/input_2_numbers_print_quotient_of_the_two_with_decimals.py
+++ Batch_1/input_2_numbers_print_quotient_of_the_two_with_decimals.py
@@ -1,3 +1,6 @@
 # input 2 numbers
+first_num = float(input("Input the 1st number: "))
+second_num = float(input("Input the 2nd number: "))
+
 # divide the two numbers
 # print the quotient with decimals
