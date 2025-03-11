d5f904136037b79325073c4c6003233adb6cf31f
my name <you@example.com>
2025-03-12 00:34:20 +0800

    division and printing of quotient with decimals

--- Batch_1/input_2_numbers_print_quotient_of_the_two_with_decimals.py
+++ Batch_1/input_2_numbers_print_quotient_of_the_two_with_decimals.py
@@ -3,4 +3,7 @@ first_num = float(input("Input the 1st number: "))
 second_num = float(input("Input the 2nd number: "))
 
 # divide the two numbers
+quotient_of_the_two = first_num / second_num
+
 # print the quotient with decimals
+print(f"The quotient of {first_num} divided by {second_num} is equals to {quotient_of_the_two}")
