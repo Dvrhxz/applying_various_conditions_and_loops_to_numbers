745646706ee9cb8d61b45639528fd9997c526d34
my name <you@example.com>
2025-03-12 00:10:04 +0800

    adding and then printing of the sum

--- Batch_1/input_2_numbers_print_the_sum_of_the_two.py
+++ Batch_1/input_2_numbers_print_the_sum_of_the_two.py
@@ -3,4 +3,7 @@ first_num = float(input("Input the 1st number: "))
 second_num = float(input("Input the 2nd number: "))
 
 # add both numbers
+sum_of_both = first_num + second_num
+
 # print the sum
+print(f"The sum of {first_num} and {second_num} is equal to {sum_of_both}.")
