61b3136c39f51f982cd7b581fd2bddebae4f3db0
my name <you@example.com>
2025-03-12 00:22:57 +0800

    multiplying and printing of the product

--- Batch_1/input_2_numbers_print_product_of_the_two.py
+++ Batch_1/input_2_numbers_print_product_of_the_two.py
@@ -3,4 +3,6 @@ first_num = float(input("Input the 1st number: "))
 second_num = float(input("Input the 2nd number: "))
 
 # multiply the two numbers
+product_of_both_numbers = first_num * second_num
 # print the product
+print(f"the product of {first_num} and {second_num} is equal to {product_of_both_numbers}")
