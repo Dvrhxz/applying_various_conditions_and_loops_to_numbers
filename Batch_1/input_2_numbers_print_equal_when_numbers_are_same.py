22fa5581e1ef0e5fddbfeef21f3ce781ba732a30
my name <you@example.com>
2025-03-12 00:00:50 +0800

    print if equal or not

--- Batch_1/input_2_numbers_print_equal_when_numbers_are_same.py
+++ Batch_1/input_2_numbers_print_equal_when_numbers_are_same.py
@@ -2,4 +2,8 @@
 first_num = float(input("Input the 1st number: "))
 second_num = float(input("Input the 2nd number: "))
 # check if numbers same
+if first_num == second_num:
     # print "equal" if numbers are same
+    print(f"{first_num} and {second_num} are equal")
+else:
+    print(f"{first_num} and {second_num} are not equal")
