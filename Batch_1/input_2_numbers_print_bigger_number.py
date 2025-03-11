5196414d94c1b3e5602fae0ece12e012f9eba3a3
my name <you@example.com>
2025-03-11 23:41:52 +0800

    2 number inputs

--- Batch_1/input_2_numbers_print_bigger_number.py
+++ Batch_1/input_2_numbers_print_bigger_number.py
@@ -1,3 +1,5 @@
 #input 2 numbers
+first_num = float(input("Input the 1st number: "))
+second_num = float(input("Input the 2nd number: "))
 #check which number is bigger
-#print the bigger number
+    #print the bigger number
