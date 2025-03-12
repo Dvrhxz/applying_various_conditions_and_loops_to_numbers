64cf536307d6936de1f163d138fff11a02722589
my name <you@example.com>
2025-03-12 11:19:33 +0800

    only print numbers not ending with 0

--- Batch_1/print_all_numbers_from_0_to_100_except_numbers_ending_with_0.py
+++ Batch_1/print_all_numbers_from_0_to_100_except_numbers_ending_with_0.py
@@ -1,4 +1,5 @@
 # make for loop printing numbers 0-100
 for numbers in range (0,101):
-    print(numbers)
     # exclude printing numbers ending in 0
+    if numbers %10 !=0:
+        print (numbers)
