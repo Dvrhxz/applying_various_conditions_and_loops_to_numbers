5fc8d209d7beca1da2032c186603cd9a9243bb21
my name <you@example.com>
2025-03-13 00:35:32 +0800

    2 input numbers

--- Batch_2/input_2_numbers_print_smaller_number.py
+++ Batch_2/input_2_numbers_print_smaller_number.py
@@ -1,3 +1,6 @@
 # input 2 numbers
+first_num = float(input("Input the 1st number: "))
+second_num = float(input("Input the 2nd number: "))
+
 # check which number is smaller
     # print the smaller number
