55243fdd722771e0781ac1df07a172a674ee9919
my name <you@example.com>
2025-03-13 01:02:35 +0800

    2 number inputs

--- Batch_2/input_2_numbers_get_remainder_when_first_number_divided_by_second_number.py
+++ Batch_2/input_2_numbers_get_remainder_when_first_number_divided_by_second_number.py
@@ -1,3 +1,6 @@
 # input 2 numbers
+first_num = float(input("Input the 1st number: "))
+second_num = float(input("Input the 2nd number: "))
+
 # get the remainder when first number is divided by the second number
 # print the remainder
