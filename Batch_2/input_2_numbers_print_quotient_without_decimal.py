1964510f52e2ebad676d3e5d44355a5eba4bd190
my name <you@example.com>
2025-03-13 00:58:23 +0800

    division and printing of quotient

--- Batch_2/input_2_numbers_print_quotient_without_decimal.py
+++ Batch_2/input_2_numbers_print_quotient_without_decimal.py
@@ -3,4 +3,7 @@ first_num = float(input("Input the 1st number: "))
 second_num = float(input("Input the 2nd number: "))
 
 # divides the two numbers and result should not have decimal
+quotient_no_decimal = first_num // second_num
+
 # print the quotient
+print(f"{first_num} divided by {second_num} without decimals is equal to {quotient_no_decimal}")
