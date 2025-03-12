b1c319d9783560e92da07bddbdf00116e6521298
my name <you@example.com>
2025-03-13 00:54:07 +0800

    2 number inputs

--- Batch_2/input_2_numbers_print_quotient_without_decimal.py
+++ Batch_2/input_2_numbers_print_quotient_without_decimal.py
@@ -1,3 +1,6 @@
 # input 2 numbers
+first_num = float(input("Input the 1st number: "))
+second_num = float(input("Input the 2nd number: "))
+
 # divides the two numbers and result should not have decimal
 # print the quotient
