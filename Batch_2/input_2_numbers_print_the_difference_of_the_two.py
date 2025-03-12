3daafd9524f980b3a800bf6f46c5b55dbec6b1c4
my name <you@example.com>
2025-03-13 00:51:09 +0800

    print the difference

--- Batch_2/input_2_numbers_print_the_difference_of_the_two.py
+++ Batch_2/input_2_numbers_print_the_difference_of_the_two.py
@@ -3,4 +3,7 @@ first_num = float(input("Input the 1st number: "))
 second_num = float(input("Input the 2nd number: "))
 
 # subtract the numbers
+difference = first_num - second_num
+
 # print the difference
+print(f"The difference of {first_num} minus {second_num} is equal to {difference}")
