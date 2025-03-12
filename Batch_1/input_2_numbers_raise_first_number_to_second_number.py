69abc870c70c6a55fe956860bb7f452d83e83578
my name <you@example.com>
2025-03-12 10:24:57 +0800

    handling exponents and printing result

--- Batch_1/input_2_numbers_raise_first_number_to_second_number.py
+++ Batch_1/input_2_numbers_raise_first_number_to_second_number.py
@@ -3,4 +3,7 @@ first_num = float(input("Input the 1st number: "))
 second_num = float(input("Input the 2nd number: "))
 
 # raise first number to second number
+result = first_num ** second_num
+
 # print result
+print(f"{first_num} raised to {second_num} is equal to {result}")
