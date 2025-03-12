c40cb37f0f947d495673ffc078551aac22154a6b
my name <you@example.com>
2025-03-13 01:20:58 +0800

    10 inputs and storing values, determining the first number in list

--- Batch_2/input_10_numbers_first_number_minus_the_remaining_numbers.py
+++ Batch_2/input_10_numbers_first_number_minus_the_remaining_numbers.py
@@ -1,4 +1,12 @@
 # make list to store values
+number_list = []
+
 # ask for 10 inputs
+for i in range(10):
+    numbers = float(input(f"Input number {i + 1}: "))
+    number_list.append(numbers)
+
+print (number_list)
+print (number_list[0])
 # get the first number then subtract using the remaining numbers
 # print the result
