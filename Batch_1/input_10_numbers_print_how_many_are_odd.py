ecb4546591b3c57063169f7bf6d6aecec8ddd58e
my name <you@example.com>
2025-03-12 10:49:41 +0800

    pseudocode

---
+++ Batch_1/input_10_numbers_print_how_many_are_odd.py
@@ -0,0 +1,5 @@
+# make list to store inputs for odds and evens
+# input 10 numbers
+    # if number odd, add to odd list
+    # if number even, add to even list
+# count and print the amount off odd numbers in the list
