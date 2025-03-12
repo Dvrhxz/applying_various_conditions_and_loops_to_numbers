ef2fd32b4b34c4c0d1880937162d29187857efc7
my name <you@example.com>
2025-03-13 01:05:56 +0800

    getting and printing of remainder

--- Batch_2/input_2_numbers_get_remainder_when_first_number_divided_by_second_number.py
+++ Batch_2/input_2_numbers_get_remainder_when_first_number_divided_by_second_number.py
@@ -3,4 +3,7 @@ first_num = float(input("Input the 1st number: "))
 second_num = float(input("Input the 2nd number: "))
 
 # get the remainder when first number is divided by the second number
+remainder = first_num %second_num
+
 # print the remainder
+print(f"The remainder when {first_num} is divided by {second_num} is equals to {remainder}")
