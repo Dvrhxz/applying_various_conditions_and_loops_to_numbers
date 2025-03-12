37927d1b72f79c6a7c3dba027f08a82942ea7cd6
my name <you@example.com>
2025-03-13 00:37:58 +0800

    print smaller number

--- Batch_2/input_2_numbers_print_smaller_number.py
+++ Batch_2/input_2_numbers_print_smaller_number.py
@@ -3,4 +3,8 @@ first_num = float(input("Input the 1st number: "))
 second_num = float(input("Input the 2nd number: "))
 
 # check which number is smaller
+if first_num < second_num:
     # print the smaller number
+    print(f"The number {first_num} is smaller than {second_num}")
+else:
+    print(f"The number {second_num} is smaller than {first_num}")
