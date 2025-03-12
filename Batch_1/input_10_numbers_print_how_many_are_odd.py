497fe15028f5be3a1ca3c5a83f82a646e5e6376f
my name <you@example.com>
2025-03-12 10:59:10 +0800

    input 10 numbers and add them to respective list

--- Batch_1/input_10_numbers_print_how_many_are_odd.py
+++ Batch_1/input_10_numbers_print_how_many_are_odd.py
@@ -1,5 +1,15 @@
 # make list to store inputs for odds and evens
+odd_numbers = []
+even_numbers = []
+
 # input 10 numbers
+for i in range (10):
+    inputs = float(input(f"input number {i + 1}: "))
     # if number odd, add to odd list
+    if inputs %2 != 0:
+        odd_numbers.append(inputs)
     # if number even, add to even list
+    else:
+        even_numbers.append(inputs)
+
 # count and print the amount off odd numbers in the list
