16121afe6d6fd782f7d853e708bc07bb2a47ffc3
my name <you@example.com>
2025-03-13 01:36:23 +0800

    subtraction of the remaining numbers and printing of results

--- Batch_2/input_10_numbers_first_number_minus_the_remaining_numbers.py
+++ Batch_2/input_10_numbers_first_number_minus_the_remaining_numbers.py
@@ -6,7 +6,11 @@ for i in range(10):
     numbers = float(input(f"Input number {i + 1}: "))
     number_list.append(numbers)
 
-print (number_list)
-print (number_list[0])
 # get the first number then subtract using the remaining numbers
+first_number = number_list[0]
+
+for numbers in number_list[1:]:
+    first_number -= numbers
+
 # print the result
+print(f"number {number_list[0]} minus all the remaining numbers is equal to {first_number}")
