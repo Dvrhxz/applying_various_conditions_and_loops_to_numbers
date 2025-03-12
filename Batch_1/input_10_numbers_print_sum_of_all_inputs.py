d8073f7a8c19828dcb9ed7df3918833afb9ea913
my name <you@example.com>
2025-03-12 10:40:10 +0800

    10 number inputs and list

--- Batch_1/input_10_numbers_print_sum_of_all_inputs.py
+++ Batch_1/input_10_numbers_print_sum_of_all_inputs.py
@@ -1,4 +1,10 @@
 # make a list to store inputs
+summation = []
+
 # input 10 numbers
+for inputs in range (10):
+    inputs = float(input(f"Input number {inputs + 1}: "))
+    summation.append(inputs)
+
 # add all numbers in the list
 # print results
