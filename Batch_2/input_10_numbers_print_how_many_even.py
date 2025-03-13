0aabb7861d402d9790fc4a56c7de6caa795d5ce2
my name <you@example.com>
2025-03-13 08:30:23 +0800

    10 inputs and storing them into a list for counting

--- Batch_2/input_10_numbers_print_how_many_even.py
+++ Batch_2/input_10_numbers_print_how_many_even.py
@@ -1,3 +1,16 @@
 # make list to store evens and odds
+even_list = []
+odd_list = []
+
 # input 10 numbers and store in list
+for i in range(10):
+    numbers = float(input(f"Input number {i + 1}: "))
+    if numbers %2 == 0:
+        even_list.append(numbers)
+    else:
+        odd_list.append(numbers)
+
+print(even_list)
+print(odd_list)
+
 # print the amount of values stored in evens list
