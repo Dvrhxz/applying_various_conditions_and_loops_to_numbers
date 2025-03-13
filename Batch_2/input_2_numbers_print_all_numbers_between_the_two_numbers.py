b9f385f4b036dda0cddd2d2fb4b6b87580726fb3
my name <you@example.com>
2025-03-14 00:04:15 +0800

    changed float to int, printing of numbers between the inputted numbers

--- Batch_2/input_2_numbers_print_all_numbers_between_the_two_numbers.py
+++ Batch_2/input_2_numbers_print_all_numbers_between_the_two_numbers.py
@@ -1,5 +1,11 @@
 # input 2 numbers
-first_num = float(input("Input the 1st number: "))
-second_num = float(input("Input the 2nd number: "))
+first_num = int(input("Input the 1st number: "))
+second_num = int(input("Input the 2nd number: "))
 
 # print numbers between the two
+num_after_first = min(first_num, second_num) + 1
+num_before_second = max(first_num, second_num)
+
+print(f"the numbers between {first_num} and {second_num} are")
+for numbers in range(num_after_first, num_before_second):
+    print(numbers)
