9d6695895e7fb050920b16c96720238db01afa2e
my name <you@example.com>
2025-03-14 12:48:35 +0800

    printing of all numbers with duplicates

--- batch_4/input_10_numbers_display_all_numbers_that_have_duplicate.py
+++ batch_4/input_10_numbers_display_all_numbers_that_have_duplicate.py
@@ -1,5 +1,17 @@
 # ask 10 inputs
+numbers_list = []
 for i in range(10):
     number = int(input(f"input number {i + 1}: "))
+    numbers_list.append(number)
 
 # print numbers that have duplicates
+duplicates = set()
+first_entry_number = set()
+
+for number in numbers_list:
+    if number in first_entry_number:
+        duplicates.add(number)
+    else:
+        first_entry_number.add(number)
+
+print(f"All the numbers with duplicates are {list(duplicates)}")
