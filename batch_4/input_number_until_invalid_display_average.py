80e8d4084d8d0a0ca3d6287347d5af34a54bc4e4
my name <you@example.com>
2025-03-14 14:17:26 +0800

    printing of average

--- batch_4/input_number_until_invalid_display_average.py
+++ batch_4/input_number_until_invalid_display_average.py
@@ -1,5 +1,6 @@
 # make list to store values
 numbers_list = []
+
 # ask for inputs
 while True:
     try:
@@ -9,6 +10,11 @@ while True:
     # stop asking for inputs when invalid
     except ValueError:
         # calculate average
+        sum_of_list = sum(numbers_list)
+        average = sum_of_list / len(numbers_list)
+
         # print average
+        print(f"\nThe average of all inputs is: {average}")
+
         # end program
         break
