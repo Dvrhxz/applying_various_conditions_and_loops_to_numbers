846ce16b1702f30b0ddca9ed76986baf040bacd6
my name <you@example.com>
2025-03-14 10:23:39 +0800

    input 10 numbers

--- Batch_3/input_10_numbers_print_all_if_it_has_duplicates_print_only_one.py
+++ Batch_3/input_10_numbers_print_all_if_it_has_duplicates_print_only_one.py
@@ -1,2 +1,8 @@
 # make 10 inputs and store in list
+numbers_list = []
+
+for i in range (10):
+    number = int(input(f"Input number {i + 1}: "))
+    numbers_list.append(number)
+
 # print number, if it has duplicates print it once only
