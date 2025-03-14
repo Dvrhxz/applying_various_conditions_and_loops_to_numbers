9f12927b4321bc6e6b4ea906e0a0cbaf123cc189
my name <you@example.com>
2025-03-14 14:14:33 +0800

    input and breaking

--- batch_4/input_number_until_invalid_display_average.py
+++ batch_4/input_number_until_invalid_display_average.py
@@ -1,6 +1,14 @@
 # make list to store values
+numbers_list = []
 # ask for inputs
-# stop asking for inputs when invalid
-# calculate average
-# print average
-# end program
+while True:
+    try:
+        number = int(input("input a number: "))
+        numbers_list.append(number)
+
+    # stop asking for inputs when invalid
+    except ValueError:
+        # calculate average
+        # print average
+        # end program
+        break
