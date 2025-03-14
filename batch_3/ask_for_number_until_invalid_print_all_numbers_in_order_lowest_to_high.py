afc5ccfe981af056541b0809206627569ea5590b
my name <you@example.com>
2025-03-14 12:10:21 +0800

    looping inputs

--- batch_3/ask_for_number_until_invalid_print_all_numbers_in_order_lowest_to_high.py
+++ batch_3/ask_for_number_until_invalid_print_all_numbers_in_order_lowest_to_high.py
@@ -1,2 +1,8 @@
 # loop ask for input
+numbers_list = []
+
+while True:
+    number = int(input("Input a number: "))
+    numbers_list.append(number)
+
     # if input invalid print sorted list and end program
