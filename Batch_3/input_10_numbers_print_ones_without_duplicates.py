cc888b2d2669599d1b7781bbbf02bbb08d9ca264
my name <you@example.com>
2025-03-14 10:08:43 +0800

    print only numbers with no duplicates

--- Batch_3/input_10_numbers_print_ones_without_duplicates.py
+++ Batch_3/input_10_numbers_print_ones_without_duplicates.py
@@ -6,4 +6,11 @@ for i in range(10):
     numbers_list.append(number)
 
 # check if number only entered once
+no_duplicates = []
+
+for number in numbers_list:
+    if numbers_list.count(number) == 1:
+        no_duplicates.append(number)
+
 # print number that only appears once
+print(f"The numbers that have no duplicates are as follows: {no_duplicates}")
