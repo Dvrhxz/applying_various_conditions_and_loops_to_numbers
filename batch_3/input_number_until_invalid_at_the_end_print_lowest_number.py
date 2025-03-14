7d5014a64a0858eb7fdb84ae04cb3b8ef5c65822
my name <you@example.com>
2025-03-14 11:53:39 +0800

    looping inputs, break when invalid

--- batch_3/input_number_until_invalid_at_the_end_print_lowest_number.py
+++ batch_3/input_number_until_invalid_at_the_end_print_lowest_number.py
@@ -1,3 +1,12 @@
 # loop asking for inputs
+numbers_list = []
+while True:
+    try:
+        number = int(input(f"Input a number: "))
+
         # find and print the lowest number from all inputs
+
     # end program when input is invalid
+    except ValueError:
+        print("That is invalid\nProgram will now stop...")
+        break
