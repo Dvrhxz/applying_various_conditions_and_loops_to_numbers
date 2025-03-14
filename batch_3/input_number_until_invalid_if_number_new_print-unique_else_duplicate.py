c718dc9fce08ae7e4e0f8160aa85e5d26cfd79fd
my name <you@example.com>
2025-03-14 11:33:17 +0800

    printing of unique and duplicates per inputted number

--- batch_3/input_number_until_invalid_if_number_new_print-unique_else_duplicate.py
+++ batch_3/input_number_until_invalid_if_number_new_print-unique_else_duplicate.py
@@ -1,12 +1,20 @@
 # make loop to ask for inputs
+number_already_entered = set()
 while True:
     try:
-        number = int(input(f"Input a number: "))
+        number = int(input(f"\nInput a number: "))
+
+        # while program running if input is new say "unique" if not "duplicate"
+        if number not in number_already_entered:
+            print("Unique")
+            number_already_entered.add(number)
+        else:
+            print("Duplicate")
 
     # when input invalid end program
     except ValueError:
         print("That is invalid\nProgram will now stop...")
         break
 
-# while program running if input is new say "unique" if not "duplicate"
+
 
