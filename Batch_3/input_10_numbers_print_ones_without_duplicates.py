a98942229e539d7fbe8a2bcb8166e98d159faf98
my name <you@example.com>
2025-03-14 10:07:44 +0800

    input 10 numbers

--- Batch_3/input_10_numbers_print_ones_without_duplicates.py
+++ Batch_3/input_10_numbers_print_ones_without_duplicates.py
@@ -1,3 +1,9 @@
 # make 10 inputs and store into list
+numbers_list = []
+
+for i in range(10):
+    number =int(input(f"Input number {i + 1}: "))
+    numbers_list.append(number)
+
 # check if number only entered once
 # print number that only appears once
