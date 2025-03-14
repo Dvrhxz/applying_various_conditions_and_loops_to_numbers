5d6563efb5100b31708cd7dc11f1e3e643ed0c78
my name <you@example.com>
2025-03-14 13:41:00 +0800

    print the highest number

--- batch_4/input_numbers_until_invalid_print_highest_number.py
+++ batch_4/input_numbers_until_invalid_print_highest_number.py
@@ -1,11 +1,14 @@
 # ask for inputs until invalid
+numbers_list = []
 while True:
     try:
         number = int(input("Input a number: "))
+        numbers_list.append(number)
 
     # when invalid print results
     except ValueError:
         print("\nThat is invalid\nProgram will now print the highest number")
+        print(f"The highest number inputted is: {max(numbers_list)}")
+
         # end program
         break
-
