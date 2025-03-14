10b96c797f55da63df5099303d6a87ea0f7cf626
my name <you@example.com>
2025-03-14 10:47:53 +0800

    printing of numbers without reiteration

--- Batch_3/input_10_numbers_print_all_if_it_has_duplicates_print_only_one.py
+++ Batch_3/input_10_numbers_print_all_if_it_has_duplicates_print_only_one.py
@@ -6,3 +6,12 @@ for i in range (10):
     numbers_list.append(number)
 
 # print number, if it has duplicates print it once only
+numbers_first_entry = []
+already_in_numbers_first_entry = set()
+
+for number in numbers_list:
+    if number not in already_in_numbers_first_entry:
+        numbers_first_entry.append(number)
+        already_in_numbers_first_entry.add(number)
+
+print(f"Numbers inputted without reiteration is: {numbers_first_entry}")
