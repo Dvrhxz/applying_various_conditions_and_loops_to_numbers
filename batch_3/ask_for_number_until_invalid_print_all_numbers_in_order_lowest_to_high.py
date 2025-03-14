11a6071813aaa0159c4e17f2156f88de9dca5f75
my name <you@example.com>
2025-03-14 12:13:36 +0800

    printing from lowest to highest, then exit program

--- batch_3/ask_for_number_until_invalid_print_all_numbers_in_order_lowest_to_high.py
+++ batch_3/ask_for_number_until_invalid_print_all_numbers_in_order_lowest_to_high.py
@@ -2,7 +2,12 @@
 numbers_list = []
 
 while True:
-    number = int(input("Input a number: "))
-    numbers_list.append(number)
+    try:
+        number = int(input("Input a number: "))
+        numbers_list.append(number)
 
     # if input invalid print sorted list and end program
+    except ValueError:
+        print("\nThe numbers sorted from lowest to highest is:")
+        print(sorted(numbers_list))
+        break
