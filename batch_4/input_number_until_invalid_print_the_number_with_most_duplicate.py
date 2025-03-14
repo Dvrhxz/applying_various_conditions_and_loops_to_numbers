0c86dc2e6ac7c6aa9e166a222e5d9a5c4cadd3f9
my name <you@example.com>
2025-03-14 13:27:52 +0800

    finding and printing of what number was inputted the most times

--- batch_4/input_number_until_invalid_print_the_number_with_most_duplicate.py
+++ batch_4/input_number_until_invalid_print_the_number_with_most_duplicate.py
@@ -1,9 +1,24 @@
+import collections
+from collections import Counter
+
 # ask for looping inputs
+numbers_list = []
 while True:
     try:
         number = int(input("Input number: "))
-# when input is invalid print number with most duplicates
-    # end program
+        numbers_list.append(number)
+
+    # end program after printing results
     except ValueError:
-        print("\nThat is invalid\nProgram will now stop...")
+
+        # when input is invalid print number with most duplicates
+        instances_of_number = Counter(numbers_list)
+
+        number_with_most_duplicates = max(instances_of_number, key=instances_of_number.get)
+        duplicate_amount = instances_of_number[number_with_most_duplicates]
+
+        print("\nThat is invalid\nProgram will now show results")
+        print(f"\nThe number with the most duplicates is {number_with_most_duplicates}"
+              f" which was inputted {duplicate_amount} times")
+        
         break
