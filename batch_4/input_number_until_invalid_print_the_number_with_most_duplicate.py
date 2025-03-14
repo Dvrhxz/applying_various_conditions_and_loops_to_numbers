25d92decb24ca05869df33c30fa75ceb835c20d0
my name <you@example.com>
2025-03-14 13:00:08 +0800

    input numbers, end when invalid

--- batch_4/input_number_until_invalid_print_the_number_with_most_duplicate.py
+++ batch_4/input_number_until_invalid_print_the_number_with_most_duplicate.py
@@ -1,3 +1,9 @@
 # ask for looping inputs
+while True:
+    try:
+        number = int(input("Input number: "))
 # when input is invalid print number with most duplicates
-    #end program
+    # end program
+    except ValueError:
+        print("\nThat is invalid\nProgram will now stop...")
+        break
